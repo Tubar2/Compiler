64432ce0a771d0d5b96dd7f0abe172b6540079bf
ricardosantos <rick.giometti@hotmail.com>
2021-03-11 11:51:22 -0300

    Simulador finalizado

---
+++ cmake-build-debug/CMakeFiles/simulador.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ricardosantos/Documents/UnB/Trab_SB_1/aux/montador/montador_aux.cpp" "/Users/ricardosantos/Documents/UnB/Trab_SB_1/cmake-build-debug/CMakeFiles/simulador.dir/aux/montador/montador_aux.cpp.o"
+  "/Users/ricardosantos/Documents/UnB/Trab_SB_1/aux/simulador/simulador_aux.cpp" "/Users/ricardosantos/Documents/UnB/Trab_SB_1/cmake-build-debug/CMakeFiles/simulador.dir/aux/simulador/simulador_aux.cpp.o"
+  "/Users/ricardosantos/Documents/UnB/Trab_SB_1/simulador.cpp" "/Users/ricardosantos/Documents/UnB/Trab_SB_1/cmake-build-debug/CMakeFiles/simulador.dir/simulador.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/simulador.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulador.dir/aux/montador/montador_aux.cpp.o"
+  "CMakeFiles/simulador.dir/aux/simulador/simulador_aux.cpp.o"
+  "CMakeFiles/simulador.dir/simulador.cpp.o"
+  "simulador"
+  "simulador.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simulador.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
